c2b5584ea2a5e9381b05168f395194a18426a837
vladk <kotolupenko.vlad@mail.ru>
2023-04-26 19:28:29 +0500

    cltkfk

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,50 @@
-import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
 
-
-    public static void print(String str) {
-        System.out.println(" привет  "+ str);
-
-
+    {
+        System.out.println(" курасавая работа № 5 ");
     }
 
-    public static void main(String[] arge) {
-        int[] list = new int[10];
-        list[0] =1 ;
-        list[1] =3;
-        list[5] = 4;
-        for (int i = 0; i < 100; i++ ){
-            if (i % 2 == 0){
-                System.out.println( i);
+    private static boolean[] list;
+
+    public static void main(String[] args) throws InterruptedException {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                numbers[i]++;
+
+                System.out.println(Arrays.toString(numbers));
+            } else {
+                System.out.println(i);
             }
         }
-        for (int i = 0; i < list.length; i++) {
-            System.out.println(list[i]);
- //           if (list.get(i) % 2 == 0) {
- //               list.set(i, list.get(i) + 1);
-//                              System.out.println("");
-            }
-
+        for (int i = 2; i < 7 && i < args.length; ) {
+            long[] arr;
+            arr = new long[0];
+            numbers.wait(arr[i]);
+            System.out.println(i);
 
         }
-
     }
+}
 
 
 
 
-//for (int i = 0; i < list.size(); i++) {
-//            if (list.get(i) % 2 == 0) {
-//                list.set(i, list.get(i) + 1);
-//
+
+
+
+
+
+
+
+
+  //  List sublist = new ArrayList<>();
+//for (int i = 2; i < 7 && i < arr.length; i++) {
+//        sublist.add(arr[i]);
+ //       }
+ //       return sublist;
+ //       }
+
